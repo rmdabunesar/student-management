8452db4926fafe23240a766ddca0d6fdf3c973cb
rmdabunesar <abunesar13@gmail.com>
2026-04-30 10:50:45 +0600

    Blade Directives

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -8,5 +8,18 @@
         <p>This is the About Us page.</p>
         <p>Name: {{ $name }}</p>
         <p>Email: {{ $email }}</p>
+
+        @for($i = 0; $i < 5; $i++)
+            <p>iteration: {{ $i }}</p>
+        @endfor
+
+        @if($name === 'Jhon-Deo')
+            <p>Hello, Jhon-Deo!</p>
+        @endif
+
+        @auth
+            <p>Welcome, {{ auth()->user()->name }}!</p>
+        @endauth
+
     </body>
 </html>
