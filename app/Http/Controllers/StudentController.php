8f94254b75a70590cb5d639a485a7ac440878050
rmdabunesar <abunesar13@gmail.com>
2026-05-02 20:57:29 +0600

    Difference between public, private, and construct functions

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,18 +1,29 @@
 <?php
-
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
+    protected $name;
+    protected $age;
+    public function __construct()
+    {
+        $this->name = 'John Doe';
+        $this->age = 20;
+    }
+
     public function index()
     {
-        return '<h1>Student Index</h1>';
+        $privateData = $this->privateMethod();
+        return $privateData . ' ' . $this->name . ' ' . $this->age;
     }
 
     public function aboutUs($id, $name)
     {
         return view('aboutus', ['id' => $id, 'name' => $name]);
+    }
+
+    private function privateMethod()
+    {
+        return 'This is a private method.';
     }
 }
