cb857c1814ec3fe8a0030da2a9fa931dac46a83e
rmdabunesar <abunesar13@gmail.com>
2026-05-03 11:57:40 +0600

    Factory (Adding Fake Data)

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'age' => $this->faker->numberBetween(18, 30),
+            'date_of_birth' => $this->faker->date('Y-m-d'),
+            'gender' => $this->faker->randomElement(['male', 'female', 'other']),
+        ];
+    }
+}
--- database/migrations/2026_05_02_153959_add_user_id_to_students.php
+++ database/migrations/2026_05_02_153959_add_user_id_to_students.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('students', function (Blueprint $table) {
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            //
         });
     }
 
@@ -22,8 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('students', function (Blueprint $table) {
-            $table->dropForeign(['user_id']);
-            $table->dropColumn('user_id');
+            //
         });
     }
 };
